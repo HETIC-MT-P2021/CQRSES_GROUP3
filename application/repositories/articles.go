c3ef86c9ce5873b32af831c6b52f172d8ae6f0a0
Edwin Vautier <edwin.vautier@hetic.net>
2021-01-25 23:31:26 +0100

    feat: create the article creation function

--- application/repositories/articles.go
+++ application/repositories/articles.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	log "github.com/sirupsen/logrus"
+	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/application/services"
 	"errors"
 	"time"
 	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/core/es"
@@ -9,23 +11,33 @@ import (
 
 // PersistArticle saves an article in es
 func PersistArticle(articleForm *models.ArticleForm) error {
+	// Validate the form struct
 	err := models.ValidateArticle(articleForm)
 	if err != nil {
 		return errors.New("error during article form validations")
 	}
 	
+	// Create the final article, event and document
 	article := models.Article {
 		AuthorID: articleForm.AuthorID,
 		Title: articleForm.Title,
 		Content: articleForm.Content,
 		CreatedAt: time.Now(),
 	}
-	event := es.Event {
-		Typology: es.Create,
-		Payload: article,
-		CreatedAt: time.Now(),
+	document := services.Document {
+		Body: es.Event {
+			Typology: es.Create,
+			Payload: article,
+			CreatedAt: time.Now(),
+		},
 	}
-
+		
+	err = services.CreateNewDocumentInIndex("article", &document)
+	if err != nil {
+		log.Error("Error while creating event : ", err)
+		return err
+	}
+	log.Info("document created : ", document)
 	return nil
 }
 
