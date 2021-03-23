172487719cf5ae8388565bc35f3ef390c01d8d2d
JackMaarek <maarekjack6@gmail.com>
2021-03-23 01:43:53 +0100

    Fix: changed directory name for consumer build.

--- consumer/main.go
+++ consumer/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/rabbitmq/consummer"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.Info("Consuming queues")
 	resultsChannel := make(chan interface{})
-	cqs := consummer.ConsumeQueueService{Queue: "CreateArticleCommand"}
+	cqs := consummer.ConsumeQueueService{Queue: "create"}
 	go cqs.Consume(resultsChannel)
 	for data := range resultsChannel {
 		fmt.Println(data)
