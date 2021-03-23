f5dbd2ddca0c9fb6d64e2ce4b8a6b65b98eeed5d
JackMaarek <maarekjack6@gmail.com>
2021-03-23 01:51:33 +0100

    New: added method to decode and persist article in es.

--- consumer/main.go
+++ consumer/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/core/es"
+	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/database"
+	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/helpers"
 	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/rabbitmq/consummer"
+	"github.com/HETIC-MT-P2021/CQRSES_GROUP3/shared/repositories"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	log.Info("Consuming queues")
+	ecfg := database.EsCfg{Url: "http://es:9200"}
+	database.GetESClient(&ecfg)
 	resultsChannel := make(chan interface{})
-	cqs := consummer.ConsumeQueueService{Queue: "create"}
+	cqs := consummer.ConsumeQueueService{Queue: string(es.Create)}
 	go cqs.Consume(resultsChannel)
 	for data := range resultsChannel {
-		fmt.Println(data)
+		decodeAndPersistArticle(data)
+	}
+}
+
+func decodeAndPersistArticle(data interface{})  {
+	var eventStruct es.Event
+	err := helpers.Decode(data, &eventStruct)
+	if err != nil {
+		log.Error("Error while decoding event: ", err)
+	}
+	log.Info(&eventStruct)
+
+	err = repositories.PersistArticleEvent(&eventStruct)
+	if err != nil {
+		log.Error("Error while persisting event: ", err)
 	}
 }
